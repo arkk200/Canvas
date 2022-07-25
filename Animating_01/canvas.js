7b92b2bad36fabc81bede52e8ad67e84d4906669
arkk200 <arkk20000@gmail.com>
2022-07-26 03:36:20 +0900

    Added random color and size, Added more circles

--- Animating_01/canvas.js
+++ Animating_01/canvas.js
@@ -11,13 +11,20 @@ let mouse = {
 }
 
 let maxRadius = 40;
-let minRadius = 2;
+
+let colorArray = [
+    '#ffaa33',
+    '#99ffaa',
+    '#00ff00',
+    '#4411aa',
+    '#ff1100',
+]
 
 window.addEventListener('mousemove', event => {
     mouse.x = event.x;
     mouse.y = event.y;
     console.log(mouse);
-})
+});
 
 function Circle(x, y, dx, dy, radius) { // 원 클래스
     this.x = x;
@@ -25,13 +32,14 @@ function Circle(x, y, dx, dy, radius) { // 원 클래스
     this.dx = dx;
     this.dy = dy;
     this.radius = radius;
+    this.minRadius = radius;
+    this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
 
     this.draw = function() { // 원을 그림
         // clearRect(x, y, width, height); 캔버스에 그려진 요소들을 다 지움
         c.beginPath();
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-        c.strokeStyle = "red";
-        c.stroke();
+        c.fillStyle = this.color;
         c.fill(); // fill() 매서드는 면을 생성한다.
     }
     this.update = function() { // 원의 방향과 속도를 업데이트함
@@ -45,18 +53,17 @@ function Circle(x, y, dx, dy, radius) { // 원 클래스
         if (mouse.x - this.x < 50 && mouse.x - this.x > -50 && mouse.y - this.y < 50 && mouse.y - this.y > -50) { // 만약 마우스와 원의 거리가 50 미만이라면
             if(this.radius < maxRadius)
             this.radius += 2;
-        } else if (this.radius > minRadius){
+        } else if (this.radius > this.minRadius){
             this.radius -= 1;
         }
     }
 }
 
-let radius = 50; // 반지름
-
 let circleArray = []; // 원 여러개를 저장하는 배열
 
-for(let i = 0; i < 300; i++){ // 원을 여러개 생성함
+for(let i = 0; i < 1000; i++){ // 원을 여러개 생성함
     // 원이 생성될 때마다 각각 랜덤함 값을 줌
+    let radius = Math.random() * 3 + 1; // 반지름에 랜덤값을 줌
     let x = Math.random() * (innerWidth - radius * 2) + radius; // 위치에 랜덤값을 줌
     let y = Math.random() * (innerHeight - radius * 2) + radius;
     let dx = (Math.random() - 0.5) * 5; // 방향(부호) 및 속도(절댓값)에 랜덤값을 줌
