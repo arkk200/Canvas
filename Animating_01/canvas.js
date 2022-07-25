4e8a07a3b80c7455abde075a12088d1e2e4731a7
arkk200 <arkk20000@gmail.com>
2022-07-26 03:24:51 +0900

    Circles are size up or down when you move your mouse

--- Animating_01/canvas.js
+++ Animating_01/canvas.js
@@ -5,6 +5,20 @@ canvas.height = window.innerHeight;
 
 let c = canvas.getContext('2d');
 
+let mouse = {
+    x: undefined,
+    y: undefined
+}
+
+let maxRadius = 40;
+let minRadius = 2;
+
+window.addEventListener('mousemove', event => {
+    mouse.x = event.x;
+    mouse.y = event.y;
+    console.log(mouse);
+})
+
 function Circle(x, y, dx, dy, radius) { // 원 클래스
     this.x = x;
     this.y = y;
@@ -26,6 +40,14 @@ function Circle(x, y, dx, dy, radius) { // 원 클래스
         if(this.y + this.radius > innerHeight || this.y - this.radius < 0) { this.dy = -this.dy; }
         this.x += this.dx;
         this.y += this.dy;
+
+        // interactivity
+        if (mouse.x - this.x < 50 && mouse.x - this.x > -50 && mouse.y - this.y < 50 && mouse.y - this.y > -50) { // 만약 마우스와 원의 거리가 50 미만이라면
+            if(this.radius < maxRadius)
+            this.radius += 2;
+        } else if (this.radius > minRadius){
+            this.radius -= 1;
+        }
     }
 }
 
@@ -33,12 +55,12 @@ let radius = 50; // 반지름
 
 let circleArray = []; // 원 여러개를 저장하는 배열
 
-for(let i = 0; i < 100; i++){ // 원을 여러개 생성함
+for(let i = 0; i < 300; i++){ // 원을 여러개 생성함
     // 원이 생성될 때마다 각각 랜덤함 값을 줌
     let x = Math.random() * (innerWidth - radius * 2) + radius; // 위치에 랜덤값을 줌
     let y = Math.random() * (innerHeight - radius * 2) + radius;
-    let dx = (Math.random() - 0.5) * 3; // 방향(부호) 및 속도(절댓값)에 랜덤값을 줌
-    let dy = (Math.random() - 0.5) * 3;
+    let dx = (Math.random() - 0.5) * 5; // 방향(부호) 및 속도(절댓값)에 랜덤값을 줌
+    let dy = (Math.random() - 0.5) * 5;
     circleArray.push(new Circle(x, y, dx, dy, radius));
 }
 
