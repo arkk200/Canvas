42dcf14eb4ed4a0a3fba1044c703e8263f737fa7
arkk200 <arkk20000@gmail.com>
2022-07-26 01:32:06 +0900

    How to draw a square

--- canvas.js
+++ canvas.js
@@ -7,3 +7,10 @@ JS에서 크기를 지정해준다.
 */
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+let c = canvas.getContext('2d');
+
+// c.fillRect(x, y, width, height); 사각형을 그려줌
+c.fillRect(100, 100, 100, 100);
+c.fillRect(400, 100, 100, 100);
+c.fillRect(300, 300, 100, 100);
