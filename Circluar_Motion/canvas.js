cfbd8137af7f1a0c18bb10ae4277dae27b730e34
arkk200 <arkk20000@gmail.com>
2022-07-27 12:38:36 +0900

    Changed arc to line, and made lineCap round

--- Circluar_Motion/canvas.js
+++ Circluar_Motion/canvas.js
@@ -9,7 +9,11 @@ const mouse = {
   y: innerHeight / 2
 };
 
-const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66'];
+const colors = [
+  '#00bdff',
+  '#4d39ce',
+  '#088eff',
+];
 
 // Event Listeners
 addEventListener('mousemove', (event) => {
@@ -51,22 +55,41 @@ class Particle {
     this.radians = Math.random() * Math.PI * 2;
     this.velocity = 0.05;
     this.distanceFromCenter = randomIntFromRange(50, 120);
+    this.lastMouse = {x: x, y: y};
   }
-
-  draw() {
+  draw({x, y}) {
     c.beginPath();
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    c.fillStyle = this.color;
-    c.fill();
+    /*
+    fillRect는 rect, arc 등과 달리 fill() 매서드를 호출하지 않아도 면이 생성된다.
+    단, fillRect() 매서드가 호출되기 전에 beginPath() 매서드가 호출된다면 fillStyle을 정의해주어야 면이 나타난다.
+    */
+    // c.fillRect(this.x, this.y, 5, 5);
+    // 원을 생성하여 애니메이션을 그리면 원들 사이에 간격이 보이기에 선을 이용함
+    // c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    c.strokeStyle = this.color;
+    c.lineWidth = this.radius;
+    c.moveTo(x, y);
+    c.lineTo(this.x, this.y);
+    c.lineCap = "round";
+    c.stroke();
     c.closePath();
   }
 
   update() {
     // Move points over time
+    const lastPoint = {x: this.x, y: this.y};
+
+    // Move points over time
     this.radians += this.velocity;
-    this.x = this.fixedX + Math.cos(this.radians) * this.distanceFromCenter;
-    this.y = this.fixedY + Math.sin(this.radians) * this.distanceFromCenter;
-    this.draw();
+
+    // Drag effect
+    this.lastMouse.x += (mouse.x - this.lastMouse.x) * 0.05;
+    this.lastMouse.y += (mouse.y - this.lastMouse.y) * 0.05;
+
+    // Circular Motion
+    this.x = this.lastMouse.x + Math.cos(this.radians) * this.distanceFromCenter;
+    this.y = this.lastMouse.y + Math.sin(this.radians) * this.distanceFromCenter;
+    this.draw(lastPoint);
   }
 }
 
@@ -75,14 +98,15 @@ let particles;
 function init() {
   particles = [];
   for(let i = 0; i < 50; i++){
-    particles.push(new Particle(canvas.width/2, canvas.height/2, 5, 'blue'));
+    const radius = randomIntFromRange(5, 10);
+    particles.push(new Particle(canvas.width/2, canvas.height/2, radius, randomColor(colors)));
   }
 }
 
 // Animation Loop
 function animate() {
   requestAnimationFrame(animate);
-  c.fillStyle = "rgba(255, 255, 255, 0.2)";
+  c.fillStyle = "rgba(255, 255, 255, 0.05)";
   c.fillRect(0, 0, canvas.width, canvas.height);
   particles.forEach(particle => {
     particle.update();
