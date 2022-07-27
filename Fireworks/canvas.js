98fcc1c3afed08efb10fe46f534f427113045f06
arkk200 <arkk20000@gmail.com>
2022-07-27 23:57:46 +0900

    Fireworks animation

--- Fireworks/canvas.js
+++ Fireworks/canvas.js
@@ -19,6 +19,9 @@ addEventListener('resize', () => {
   init();
 });
 
+const gravity = 0.03;
+const friction = 0.99;
+
 function randomIntFromRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
@@ -36,23 +39,34 @@ function distance(x1, y1, x2, y2) {
 
 // Objects
 class Particle {
-  constructor(x, y, radius, color) {
+  constructor(x, y, radius, color, velocity) {
     this.x = x;
     this.y = y;
     this.radius = radius;
     this.color = color;
+    this.velocity = velocity;
+    this.alpha = 1;
   }
 
   draw() {
+    c.save() // globalAlpha() 매서드가 호출되기 전 상태 저장
+    c.globalAlpha = this.alpha;
     c.beginPath();
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
     c.fillStyle = this.color;
     c.fill();
     c.closePath();
+    c.restore();
   }
 
   update() {
     this.draw();
+    this.velocity.x *= friction;
+    this.velocity.y *= friction;
+    this.velocity.y += gravity;
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+    this.alpha -= 0.005;
   }
 }
 
@@ -60,15 +74,42 @@ class Particle {
 let particles;
 function init() {
   particles = [];
-
-  for(let i = 0; i < 400; i++){
-  }
 }
 // Animation Loop
 function animate() {
   requestAnimationFrame(animate);
-  c.clearRect(0, 0, canvas.width, canvas.height);
+  c.fillStyle = 'rgba(0, 0, 0, 0.05)'
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  particles.forEach((particle, i) => {
+    if(particle.alpha > 0)
+      particle.update();
+    else {
+        particles.splice(i, 1);
+    }
+  })
 }
 
 init();
 animate();
+
+addEventListener('click', (event) => {
+    mouse.x = event.clientX;
+    mouse.y = event.clientY;
+
+    const particleCount = 400;
+    const angleIncrement = Math.PI * 2 / particleCount;
+    const power = 15;
+
+    for(let i = 0; i < particleCount; i++){
+        particles.push(new Particle(
+            mouse.x,
+            mouse.y,
+            5,
+            `hsl(${Math.random() * 360}, 50%, 50%)`,
+            {
+                x: Math.cos(angleIncrement * i) * Math.random() * power,
+                y: Math.sin(angleIncrement * i) * Math.random() * power
+            }
+        ))
+    }
+})
