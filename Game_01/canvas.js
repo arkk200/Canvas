caf70624f70200c059b235110dd5644f8d282756
arkk200 <arkk20000@gmail.com>
2022-07-29 04:39:54 +0900

    Finished 'Shoot projectiles'

--- Game_01/canvas.js
+++ Game_01/canvas.js
@@ -1,7 +1,7 @@
 /*
 Basic Game Checklist:
 - Create a player 플레이어 생성하기 V
-- Shoot projectiles 발사체 생성 및 쏘기
+- Shoot projectiles 발사체 생성 및 쏘기 V
 - Create enemies 적 생성하기
 - Detect collision on enemy / projectile hit 적, 발사체 충돌 감지하기
 - Detect collision on enemy / player hit 적, 플레이어 충돌 감지하기
@@ -37,8 +37,64 @@ class Player {
     }
 }
 
+class Projectile {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+    }
+    draw() {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.fillStyle = this.color;
+        c.fill();
+    }
+
+    update() {
+        this.draw();
+        this.x += this.velocity.x;
+        this.y += this.velocity.y;
+    }
+}
+
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
 const player = new Player(x, y, 30, 'blue');
-player.draw();
+
+const projectiles = []; // 발사체 그룹
+
+function animate() {
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    player.draw();
+    projectiles.forEach(projectile => {
+        projectile.update();
+    })
+}
+
+addEventListener('click', event => {
+    const angle = Math.atan2(
+        event.clientY - canvas.height / 2,
+        event.clientX - canvas.width / 2
+    );
+    const velocity = {
+        x: Math.cos(angle),
+        y: Math.sin(angle)
+    }
+    projectiles.push(new Projectile(
+            canvas.width / 2,
+            canvas.height / 2,
+            5,
+            'red',
+            {
+                x: velocity.x,
+                y: velocity.y
+            }
+        )
+    );
+});
+
+animate();
