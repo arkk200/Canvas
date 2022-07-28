005b891e9210c43ad5e98851ff5dc84f057ca3c3
arkk200 <arkk20000@gmail.com>
2022-07-29 05:16:15 +0900

    Finished 'Create enemies'

--- Game_01/canvas.js
+++ Game_01/canvas.js
@@ -2,7 +2,7 @@
 Basic Game Checklist:
 - Create a player 플레이어 생성하기 V
 - Shoot projectiles 발사체 생성 및 쏘기 V
-- Create enemies 적 생성하기
+- Create enemies 적 생성하기 V
 - Detect collision on enemy / projectile hit 적, 발사체 충돌 감지하기
 - Detect collision on enemy / player hit 적, 플레이어 충돌 감지하기
 - Remove off screen projectiles 스크린에서 벗어난 발사체 삭제하기
@@ -59,12 +59,61 @@ class Projectile {
     }
 }
 
+class Enemy {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+    }
+    draw() {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.fillStyle = this.color;
+        c.fill();
+    }
+
+    update() {
+        this.draw();
+        this.x += this.velocity.x;
+        this.y += this.velocity.y;
+    }
+}
+
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
 const player = new Player(x, y, 30, 'blue');
-
 const projectiles = []; // 발사체 그룹
+const enemies = []; // 적 그룹
+
+function spawnEnemies() {
+    setInterval(() => {
+        const radius = Math.random() * (30 - 4) + 4;
+
+        let x;
+        let y;
+
+        if(Math.random() < 0.5){
+            x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
+            y = Math.random() * canvas.height;
+        } else {
+            x = Math.random() * canvas.width;
+            y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
+        }
+        const color = 'green';
+        const angle = Math.atan2(
+            canvas.height / 2 - y,
+            canvas.width / 2 - x
+        );
+        const velocity = {
+            x: Math.cos(angle),
+            y: Math.sin(angle)
+        };
+        enemies.push(new Enemy(x, y, radius, color, velocity));
+    }, 1000);
+}
 
 function animate() {
     requestAnimationFrame(animate);
@@ -72,6 +121,10 @@ function animate() {
     player.draw();
     projectiles.forEach(projectile => {
         projectile.update();
+    });
+
+    enemies.forEach(enemy => {
+        enemy.update();
     })
 }
 
@@ -83,7 +136,7 @@ addEventListener('click', event => {
     const velocity = {
         x: Math.cos(angle),
         y: Math.sin(angle)
-    }
+    };
     projectiles.push(new Projectile(
             canvas.width / 2,
             canvas.height / 2,
@@ -98,3 +151,4 @@ addEventListener('click', event => {
 });
 
 animate();
+spawnEnemies();
