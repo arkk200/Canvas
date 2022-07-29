deb5bd1e669a787885ceda61f1caf859cf23fb95
arkk200 <arkk20000@gmail.com>
2022-07-29 16:17:11 +0900

    Finished 'Create particle explosion on hit'

--- Game_01/canvas.js
+++ Game_01/canvas.js
@@ -8,7 +8,7 @@ Basic Game Checklist:
 - Remove off screen projectiles 스크린에서 벗어난 발사체 삭제하기 V
 - Colorize game 색 입히기 V
 - Shrink enemies on hit 맞힌 적 수축하기 V
-- Create particle explosion on hit 맞았을 때 터지는 파티클 생성하기
+- Create particle explosion on hit 맞았을 때 터지는 파티클 생성하기 V
 - Add score 점수 추가하기
 - Add game over UI 게임 오버 UI 추가하기
 - Add restart button 재시작 버튼 추가하기
@@ -81,12 +81,43 @@ class Enemy {
     }
 }
 
+const friction = 0.99;
+class Particle {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+        this.alpha = 1;
+    }
+    draw() {
+        c.save();
+        c.globalAlpha = this.alpha;
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.fillStyle = this.color;
+        c.fill();
+        c.restore();
+    }
+
+    update() {
+        this.draw();
+        this.velocity.x *= friction;
+        this.velocity.y *= friction;
+        this.x += this.velocity.x;
+        this.y += this.velocity.y;
+        this.alpha -= 0.01;
+    }
+}
+
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
 const player = new Player(x, y, 10, 'white');
 const projectiles = []; // 발사체 그룹
 const enemies = []; // 적 그룹
+const particles = [];
 
 function spawnEnemies() {
     setInterval(() => {
@@ -95,7 +126,7 @@ function spawnEnemies() {
         let x;
         let y;
 
-        if(Math.random() < 0.5){
+        if (Math.random() < 0.5) {
             x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
             y = Math.random() * canvas.height;
         } else {
@@ -121,6 +152,13 @@ function animate() {
     c.fillStyle = 'rgba(0, 0, 0, 0.1)';
     c.fillRect(0, 0, canvas.width, canvas.height);
     player.draw();
+    particles.forEach((particle, index) => {
+        if(particle.alpha <= 0) {
+            particles.splice(index, 1);
+            return;
+        }
+        particle.update();
+    })
     projectiles.forEach((projectile, index) => {
         projectile.update();
 
@@ -138,7 +176,7 @@ function animate() {
         enemy.update();
 
         const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y);
-        if(dist - enemy.radius - player.radius < 0) {
+        if (dist - enemy.radius - player.radius < 0) {
             cancelAnimationFrame(animationId);
         }
 
@@ -146,25 +184,37 @@ function animate() {
             const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y);
 
             // when projectiles touch enemy
-            if(dist - enemy.radius - projectile.radius < 0){
+            if (dist - enemy.radius - projectile.radius < 0) {
+                for (let i = 0; i < enemy.radius * 2; i++) {
+                    particles.push(new Particle(
+                        projectile.x,
+                        projectile.y,
+                        Math.random() * 2,
+                        enemy.color,
+                        {
+                            x: (Math.random() - 0.5) * (Math.random() * 6),
+                            y: (Math.random() - 0.5) * (Math.random() * 6)
+                        }
+                    ));
+                }
                 /*
                     마지막 인수가 제거 되었는데도
                     그릴려고 하는 것 때문에 발생하는 플래시 현상을
                     setTimeout 비동기 함수로 제거함
                 */
-               if(enemy.radius - 10 > 5){
-                gsap.to(enemy, {
-                    radius: enemy.radius - 10
-                });
-                setTimeout(() => {
-                    projectiles.splice(projectileIndex, 1);
-                }, 0);
-               } else {
-                setTimeout(() => {
-                    enemies.splice(index, 1);
-                    projectiles.splice(projectileIndex, 1);
-                }, 0);
-               }
+                if (enemy.radius - 10 > 5) {
+                    gsap.to(enemy, {
+                        radius: enemy.radius - 10
+                    });
+                    setTimeout(() => {
+                        projectiles.splice(projectileIndex, 1);
+                    }, 0);
+                } else {
+                    setTimeout(() => {
+                        enemies.splice(index, 1);
+                        projectiles.splice(projectileIndex, 1);
+                    }, 0);
+                }
             }
         });
     })
@@ -180,15 +230,15 @@ addEventListener('click', event => {
         y: Math.sin(angle) * 6
     };
     projectiles.push(new Projectile(
-            canvas.width / 2,
-            canvas.height / 2,
-            5,
-            'white',
-            {
-                x: velocity.x,
-                y: velocity.y
-            }
-        )
+        canvas.width / 2,
+        canvas.height / 2,
+        5,
+        'white',
+        {
+            x: velocity.x,
+            y: velocity.y
+        }
+    )
     );
 });
 
